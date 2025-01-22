818c5ed0cd9dc06b503254274fee458ca3defaa2
vbshkn <pika_pikabu@vk.com>
2025-01-22 13:26:39 +0700

    create 9 buttons on the learning screen

--- talk/src/main/java/com/talk/LearningScreen.java
+++ talk/src/main/java/com/talk/LearningScreen.java
@@ -1,5 +1,6 @@
 package com.talk;
 
+import java.awt.*;
 import javax.swing.*;
 
 public class LearningScreen extends JFrame {
@@ -7,7 +8,22 @@ public class LearningScreen extends JFrame {
     public LearningScreen(MainScreen scr){
         this.scr = scr;
         this.setTitle("Режим заучивания");
-        this.setSize(750, 750);
+        this.setSize(900, 750);
         this.setResizable(false);
+        add(createPanel());
+    }
+
+    private JPanel createPanel(){
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(3, 3));
+        for(int i = 1; i <= 9; i++){
+            JButton newButton = new JButton("Button "+i);
+            newButton.setFont(MainScreen.loadFont().deriveFont(35f));
+            newButton.setBackground(MainScreen.THEME.getMainColor2());
+            newButton.setForeground(MainScreen.THEME.getMainTextColor());
+            newButton.setBorderPainted(false);
+            panel.add(newButton);
+        }
+        return panel;
     }
 }
