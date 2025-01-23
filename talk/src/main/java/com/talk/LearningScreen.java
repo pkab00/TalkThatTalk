c7d8db22a3af46fe69caa67c5f87e4e50d3b3eac
vbshkn <pika_pikabu@vk.com>
2025-01-23 12:22:11 +0700

    Cardgame realisation (without updating the screen after winning)

--- talk/src/main/java/com/talk/LearningScreen.java
+++ talk/src/main/java/com/talk/LearningScreen.java
@@ -4,52 +4,91 @@ import java.awt.*;
 import java.sql.*;
 import javax.swing.*;
 import java.util.*;
+import java.util.List;
 
-public class LearningScreen extends JFrame {
+public class LearningScreen extends CoreScreen {
     private final int NUM_CARDS = 6;
+    private final int NUM_COLS = 3;
+    private int LEARNED_CARDS = 0;
+    private JButton PICKED_CARD = null;
     private MainScreen scr;
     public LearningScreen(MainScreen scr){
         this.scr = scr;
         this.setTitle("Режим заучивания");
-        this.setSize(900, 750);
+        this.setSize(NUM_COLS*300, 750);
         this.setResizable(true);
         add(createPanel());
     }
 
     private JPanel createPanel(){
+        Map<String,JButton> buttonMap = new HashMap<>();
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(2, 3));
-        ArrayList<Map<String,String>> preprocessedData = prepareData();
-        Collections.shuffle(preprocessedData);
-        ArrayList<String> processedData = new ArrayList<>();
-        for(int i = 0; i < preprocessedData.size(); i++){
-            processedData.addAll(preprocessedData.get(i).keySet());
-            processedData.addAll(preprocessedData.get(i).values());
-        }
-        Collections.shuffle(processedData);
+        panel.setLayout(new GridLayout(NUM_CARDS/NUM_COLS, NUM_COLS));
+        HashMap<String,String> preprocessedData = prepareData();
+        ArrayList<String> processedData = processData(preprocessedData);
         for(int i = 0; i < NUM_CARDS; i++){
             JButton newButton = new JButton(processedData.get(i));
+            newButton.addActionListener((e) -> {
+                if(PICKED_CARD == null){
+                    PICKED_CARD = newButton;
+                    newButton.setBackground(MainScreen.THEME.getMainColor1());
+                }
+                else{
+                    String TEXT = newButton.getText();
+                    String PICKED_TEXT = PICKED_CARD.getText();
+                    if((preprocessedData.containsKey(PICKED_TEXT) && preprocessedData.get(PICKED_TEXT).equals(TEXT)) 
+                    || (preprocessedData.containsKey(TEXT) && preprocessedData.get(TEXT).equals(PICKED_TEXT))){
+                        newButton.setBackground(MainScreen.THEME.getMainColor1());
+                        newButton.setText("<html><s>"+TEXT+"</html></s>");
+                        PICKED_CARD.setText("<html><s>"+PICKED_TEXT+"</html></s>");
+                        newButton.setEnabled(false);
+                        PICKED_CARD.setEnabled(false);
+                        PICKED_CARD = null;
+                        LEARNED_CARDS++;
+                        if(LEARNED_CARDS == NUM_CARDS/2){
+                            // todo
+                        }
+                    }
+                }
+            });
             newButton.setFont(MainScreen.loadFont().deriveFont(30f));
             newButton.setBackground(MainScreen.THEME.getMainColor2());
             newButton.setForeground(MainScreen.THEME.getMainTextColor());
             newButton.setBorderPainted(false);
+            buttonMap.put(newButton.getText(), newButton);
             panel.add(newButton);
         }
         return panel;
     }
 
-    private ArrayList<Map<String,String>> prepareData(){
-        ArrayList<Map<String,String>> resList = new ArrayList<>();
+    private HashMap<String,String> prepareData(){
+        HashMap<String,String> resList = new HashMap<>();
         try{
             ResultSet set = new DBManager().getRusAndKor();
             while(set.next()){
-                Map<String,String> pair = Map.of(set.getString("rus"), set.getString("kor"));
-                resList.add(pair);
+                resList.put(set.getString("rus"), set.getString("kor"));
             }
-        }catch(SQLException e){
+        } catch(SQLException e){
             e.printStackTrace();
         }
-        Collections.shuffle(resList);
-        return new ArrayList<>(resList.stream().limit(NUM_CARDS/2).toList());
+        ArrayList<Map.Entry<String, String>> entryList = new ArrayList<>(resList.entrySet().stream().toList());
+        Collections.shuffle(entryList);
+        HashMap<String,String> outputMap = new HashMap<>();
+        for(int i = 0; i < NUM_CARDS/2; i++){
+            outputMap.put(entryList.get(i).getKey(), entryList.get(i).getValue());
+        };
+        return outputMap;
+    }
+
+    private ArrayList<String> processData(HashMap<String,String> preprocessedData){
+        ArrayList<String> processedData = new ArrayList<>();
+        List<Map.Entry<String,String>> entryList = preprocessedData.entrySet().stream().toList();
+        for(int i = 0; i < preprocessedData.size(); i++){
+            Map.Entry<String,String> entry = entryList.get(i);
+            processedData.add(entry.getKey());
+            processedData.add(entry.getValue());
+        }
+        Collections.shuffle(processedData);
+        return processedData;
     }
 }
