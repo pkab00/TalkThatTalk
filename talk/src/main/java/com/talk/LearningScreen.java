2f6d377423692fd83b14f5a619060abbb8771c3e
vbshkn <pika_pikabu@vk.com>
2025-01-28 17:07:46 +0700

    Warning fix 2

--- talk/src/main/java/com/talk/LearningScreen.java
+++ talk/src/main/java/com/talk/LearningScreen.java
@@ -1,11 +1,7 @@
 package com.talk;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
+import java.awt.event.*;
 import java.sql.*;
 
 import javax.swing.*;
